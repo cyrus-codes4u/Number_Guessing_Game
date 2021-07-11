0f892205e3a9804865e29780948c4ba4ec4be1c5
Cyrus Glanzer <76062775+cyrus-codes4u@users.noreply.github.com>
2021-07-11 06:12:25 -0400

    Modularized code to separated functions

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import random
-# Include an ASCII art logo.
+from logo import logo
 
+def set_difficulty():
+  level = input ("What level would you like to play: easy, medium, or hard? ")
+  print("I'm thinking of a number between 1 and 100")
 
-level = input ("What level would you like to play: easy, medium, or hard? ")
-print("I'm thinking of a number between 1 and 100")
-
-
-if level == "easy":
-  guesses = 10
-elif level == "medium":
-  guesses = 7
-else:
-  guesses = 5
-
-number = random.randint(1,100)
-correct = False
-
-while guesses > 0 and correct == False:
-  print(f"You have {guesses} guesses left")
-  guess = int(input("What number am I thinking of? "))
-  guesses = guesses - 1
-  if guess == number:
-    print("You got it!")
-    correct = True
+  if level == "easy":
+    return 10
+  elif level == "medium":
+    return 7
   else:
-    if guesses == 0:
-      print("You lost")
-    elif guess > number:
+    return 5
+
+def check_guess(answer,guess,guesses):
+  if guess > answer:
       print("Too high")
-    else:
+  elif guess < answer:
       print("Too low")
+  else:
+    print("You got it!")
+  return guesses - 1
+
+
+def game():
+  print(logo)
+  print ("Welcome to the number guessing game")
+  number = random.randint(1,100)
+  guesses = set_difficulty()
+  guess = 0
+
+  while guesses > 0 and guess != number:
+    print(f"You have {guesses} guesses left")
+    guess = int(input("What number am I thinking of? "))
+    guesses = check_guess(number, guess, guesses)
+    if guesses == 0:
+      print("You've lost")
+
+game()
+    
+  
+
+
+
